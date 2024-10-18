a10abe79e943052ca7723a0c9073f4a187904506
Junaid Khan <junaidkhan@Junaids-MacBook-Pro.local>
2024-10-18 22:39:52 +0500

    Add a package

--- Package/ExerciseKit/Package.swift
+++ Package/ExerciseKit/Package.swift
@@ -5,11 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "ExerciseKit",
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ExerciseKit",
             targets: ["ExerciseKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.14.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -19,5 +23,12 @@ let package = Package(
         .testTarget(
             name: "ExerciseKitTests",
             dependencies: ["ExerciseKit"]),
+        .testTarget(name: "ExerciseKitSnapshotTests",
+                    dependencies: [
+                        "ExerciseKit",
+                        .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+                    ],
+                   exclude: ["__Snapshots__"]
+                   )
     ]
 )
