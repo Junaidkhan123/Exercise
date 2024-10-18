64c25aeee2f9f6495614020db589d9038f5953ec
Junaid Khan <junaidkhan@Junaids-MacBook-Pro.local>
2024-10-18 22:00:10 +0500

    created demo project

---
+++ ExerciseDemo/ExerciseDemo/ExerciseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseDemoApp.swift
+//  ExerciseDemo
+//
+//  Created by Junaid Khan on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
