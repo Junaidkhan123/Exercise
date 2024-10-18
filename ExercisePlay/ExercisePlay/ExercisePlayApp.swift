24fe863bffddf85065bf9ca4a0f8adf05ae890c4
Junaid Khan <junaidkhan@Junaids-MacBook-Pro.local>
2024-10-18 22:03:24 +0500

    Added ExercisePlay

---
+++ ExercisePlay/ExercisePlay/ExercisePlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExercisePlayApp.swift
+//  ExercisePlay
+//
+//  Created by Junaid Khan on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExercisePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
